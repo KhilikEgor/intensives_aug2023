d7a9fce31195a81c3e9a78d215360b255a8cf889
khilikegor <khilikegor@yandex.ru>
2023-08-14 20:15:13 +0300

    6 task Done

--- src/main/java/ru/yandex/tasks/Task6BraceBalance.java
+++ src/main/java/ru/yandex/tasks/Task6BraceBalance.java
@@ -1,5 +1,9 @@
 package ru.yandex.tasks;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class Task6BraceBalance {
     public static boolean checkBalance(String sequence) {
         /*
@@ -7,7 +11,29 @@ public class Task6BraceBalance {
          * Выход: true/false, является ли строка ПСП
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return true;
+        if(sequence.length() % 2 != 0){
+            return false;
+        }
+        Map<Character, Character> bracketPairs = new HashMap<>();
+        bracketPairs.put(')', '(');
+        bracketPairs.put('}', '{');
+        bracketPairs.put(']', '[');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : sequence.toCharArray()){
+            if(bracketPairs.containsValue(c)){
+                stack.push(c);
+            } else if (bracketPairs.containsKey(c)){
+                if(stack.isEmpty() || stack.pop() != bracketPairs.get(c)){
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
     }
 
     public static void selfCheck() {
